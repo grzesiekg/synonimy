f4d31f90b38d95d8945a6ef4e65dd943952ce886
grzesiekg@home.pl <ja@ja-Lenovo-B570.(none)>
2013-02-09 12:22:41 +0100

    3

--- synonimy.pl
+++ synonimy.pl
@@ -1,5 +1,6 @@
 use CGI qw/:standard/;
 use Switch;
+use DBI;
 
 #stworzenie strony z formularzem wyboru operacji
 print
@@ -10,6 +11,8 @@ print
 	"Wybierz operację",
 	popup_menu(-name=>'operacja',
 			-values=>['szukaj', 'dodaj','usuń']),p,
+	"Wyraz główny ", textfield('wyraz1'),p,
+	"Alternatywa", textfield('wyraz2'),p,
 	submit('Wybierz'),
 	defaults('Wyczyść'),
 	end_form,
@@ -18,9 +21,27 @@ hr,"\n";
 if (param)
 {
 	$x=param('operacja');
+	$w1=param('wyraz1');
+	$w2=param('wyraz2');
 	switch($x)
 	{
-		case "szukaj" {print "szukaj"}
+		case "szukaj" {print "szukaj";
+			$dbh = DBI->connect('dbi:mysql:grzesiekg','grzesiekg','perltest123')
+			or die "Connection Error: $DBI::errstr\n";
+			$sth = $dbh->prepare( "
+		            SELECT wyraz1, wyraz2, wyraz3
+		            FROM synonimy
+		            WHERE wyraz1 = " . $dbh->quote( $w1 ) . "
+				OR wyraz2 = " . $dbh->quote( $w1 ) . "
+				OR wyraz3 = " . $dbh->quote( $w1 ) . "
+			        " );
+			$sth->execute
+			or die "SQL Error: $DBI::errstr\n";
+			while (@row = $sth->fetchrow_array) {
+			print "@row \n";
+			}
+			
+		}
 		case "dodaj" {print "dodaj"}
 		case "usuń" {print "usuń"}
 	}
