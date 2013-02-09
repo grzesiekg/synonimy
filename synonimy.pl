80282b326bc910d165ad6255d76bf929c0057b07
grzesiekg@home.pl <ja@ja-Lenovo-B570.(none)>
2013-02-09 12:52:11 +0100

    szukaj beta done

--- synonimy.pl
+++ synonimy.pl
@@ -2,7 +2,7 @@ use CGI qw/:standard/;
 use Switch;
 use DBI;
 
-#stworzenie strony z formularzem wyboru operacji
+#stworzenie strony z formularzem wyboru operacji i polami do wpisywania wyrazów
 print
 	header(-charset=>'utf-8'),
 	start_html('Synonimy'),
@@ -20,14 +20,20 @@ hr,"\n";
 
 if (param)
 {
+#przypisanie wybranej operacji i wyrazów do zmiennych
 	$x=param('operacja');
 	$w1=param('wyraz1');
 	$w2=param('wyraz2');
+	$licznik=0;
+#uruchomienie czynności w zależności od operacji
 	switch($x)
 	{
-		case "szukaj" {print "szukaj";
+		case "szukaj" {
+#ustanowienie połączenia z bazą danych, przygotowanie zapytania do wykonania w bazie
 			$dbh = DBI->connect('dbi:mysql:grzesiekg','grzesiekg','perltest123')
 			or die "Connection Error: $DBI::errstr\n";
+			$dbh->{'mysql_enable_utf8'} = 1;
+			$dbh->do('SET NAMES utf8');
 			$sth = $dbh->prepare( "
 		            SELECT wyraz1, wyraz2, wyraz3
 		            FROM synonimy
@@ -37,8 +43,17 @@ if (param)
 			        " );
 			$sth->execute
 			or die "SQL Error: $DBI::errstr\n";
+#wyświetlenie wyników zapytania lub informacji o ich braku
 			while (@row = $sth->fetchrow_array) {
-			print "@row \n";
+				print "@row \n";
+				$licznik++;
+			}
+			if ($licznik==0)
+			{
+				print "Nie znaleziono wyrazu"
+			} else
+			{
+				$licznik=0;
 			}
 			
 		}
